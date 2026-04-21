fc02db6434e367db61a722b0f2e4c2c6f7c27858
Kamal Gorka <kamal.kumar@midlandmicrofin.com>
2026-04-21 13:30:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 from ui import load_global_css
+
+# ✅ MUST BE FIRST
+st.set_page_config(page_title="MML Smart Reports", layout="wide")
+
+# ✅ AFTER that
 load_global_css()
 
-st.set_page_config(page_title="MML Smart Reports", layout="wide")
 st.title("📊 MML Smart Reports")
 st.write("Select a team from the left sidebar.")
-
